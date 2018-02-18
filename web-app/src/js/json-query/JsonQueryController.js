32c164a2c49bd85596f03a62b6a3281241dd87a7
Ashwani Kumar <ashwanikumar04@gmail.com>
2018-02-19 00:57:52 +0530

    added code

--- web-app/src/js/json-query/JsonQueryController.js
+++ web-app/src/js/json-query/JsonQueryController.js
@@ -13,23 +13,21 @@
                     Flash.create('danger', "Some error occurred while loading the library", 'custom-class');
                 });
         };
-        var formatJson = function (data) {
+        var formatObject = function (data) {
             return JSON.stringify(data, null, 4);
         };
         $scope.format = function () {
-            $scope.input = formatJson(JSON.parse($scope.input));
+            $scope.input = formatObject(JSON.parse($scope.input));
         };
         $scope.loadLibrary = function () {
             loadLibrary($scope.selectedLibrary.url);
         };
 
-        // _.forEach(input,function(o){
-        //     console.log(o); 
-        //  });
         $scope.run = function () {
             var input = JSON.parse($scope.input);
-            var F = new Function("input", $scope.rightEditor);
-            return (F(input));
+            var result = eval($scope.rightEditor);
+            $scope.resultEditor = formatObject(result);
+
         };
         $scope.generateGist = function () {
             if (!$scope.rightEditor) {
@@ -47,7 +45,7 @@
                 });
         };
         loadLibrary($scope.libraries[0].url);
-        $scope.input = formatJson(jsonQueryService.getDefaultObject());
+        $scope.input = formatObject(jsonQueryService.getDefaultObject());
     }
 
     angular.module('jsonQuery.controllers.query', [])
